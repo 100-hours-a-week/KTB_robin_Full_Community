74066186c65d9a327626473342b2551dcf09bcab
dragonwaterr <yongsu626@naver.com>
2025-10-21 08:50:41 +0900

    refactor: 파일과 관련한 여러가지 에러를 담기 위해 에러 enum 소스이름 수정

--- src/main/java/ktb3/fullstack/week4/common/error/codes/FileError.java
+++ src/main/java/ktb3/fullstack/week4/common/error/codes/FileError.java
@@ -7,11 +7,11 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum FileUploadError implements ErrorCode {
+public enum FileError implements ErrorCode {
 
     IMAGE_SIZE_TOO_BIG (HttpStatus.BAD_REQUEST, "image_size_too_big"),
-    INVALID_FILE_TYPE (HttpStatus.BAD_REQUEST, "invalid_file_type");
-
+    INVALID_FILE_TYPE (HttpStatus.BAD_REQUEST, "invalid_file_type"),
+    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "image_not_found");
     private final HttpStatus status;
     private final String message;
 
