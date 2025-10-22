47693792f260e880703ccd0fca66a3d37341fc74
dragonwaterr <yongsu626@naver.com>
2025-10-22 15:05:34 +0900

    feat: 유효하지 않은 게시글 id 입력 에러코드에 추가

--- src/main/java/ktb3/fullstack/week4/common/error/codes/PostError.java
+++ src/main/java/ktb3/fullstack/week4/common/error/codes/PostError.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum PostError implements ErrorCode {
 
+    POST_ID_INVALID(HttpStatus.BAD_REQUEST, "post_id_is_invalid"),
     CANNOT_FOUND_POST(HttpStatus.NOT_FOUND, "cannot_found_post"),
     CANNOT_EDIT_OTHERS_POST(HttpStatus.FORBIDDEN, "cannot_edit_others_post"),
     CANNOT_DELETE_OTHERS_POST(HttpStatus.FORBIDDEN, "cannot_delete_others_post");
