7052f61eb7b8cf15b89e7ea9dcd9cf22e0e188b4
dragonwaterr <yongsu626@naver.com>
2025-10-29 20:49:09 +0900

    feat: 생성, 수정일시 속성을 담은 추상 클래스 추가

--- src/main/java/ktb3/fullstack/week4/domain/BaseModifiedTimeEntity.java
+++ src/main/java/ktb3/fullstack/week4/domain/BaseModifiedTimeEntity.java
@@ -3,7 +3,7 @@ package ktb3.fullstack.week4.domain;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -11,8 +11,8 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseTimeEntity {
+public abstract class BaseModifiedTimeEntity extends BaseTimeEntity {
 
-    @CreatedDate
-    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 }
