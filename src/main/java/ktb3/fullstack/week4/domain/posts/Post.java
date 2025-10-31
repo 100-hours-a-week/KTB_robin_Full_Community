74a540d43ec64113c3eda92db87ab4101e73ae57
dragonwaterr <yongsu626@naver.com>
2025-10-31 20:21:24 +0900

    refactor: 중복되는 userId 는 속성은 외래키로 대체

--- src/main/java/ktb3/fullstack/week4/domain/posts/Post.java
+++ src/main/java/ktb3/fullstack/week4/domain/posts/Post.java
@@ -29,10 +29,6 @@ public class Post extends SoftDeletetionEntity {
     private Long id;
 
     @NonNull
-    @Column(name = "author_id")
-    private Long authorId; // author가 이름을 변경할 수 있기 때문에, authorId(userId) 토큰에서 꺼내쓸 예정
-
-    @NonNull
     @Column(name = "title")
     private String title;
 
