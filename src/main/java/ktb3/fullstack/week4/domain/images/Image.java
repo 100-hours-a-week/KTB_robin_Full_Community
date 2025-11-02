7061df77fc4a7ecb99fb085828a324eb43e3ab5f
dragonwaterr <yongsu626@naver.com>
2025-11-03 08:54:22 +0900

    feat: 이미지 부모클래스/테이블에, 대표이미지 여부. 게시순서 속성.컬럼 추가

--- src/main/java/ktb3/fullstack/week4/domain/images/Image.java
+++ src/main/java/ktb3/fullstack/week4/domain/images/Image.java
@@ -1,12 +1,25 @@
 package ktb3.fullstack.week4.domain.images;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 import ktb3.fullstack.week4.domain.SoftDeletetionEntity;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+@MappedSuperclass
+@SuperBuilder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class Image extends SoftDeletetionEntity {
 
-    @Column(name = "image_url")
-    private String imageUrl;
+    @Column(name = "image_url", unique = true)
+    protected String imageUrl;
+
+    @Column(name = "is_primary")
+    protected boolean isPrimary;
+
+    @Column(name = "display_order")
+    protected int displayOrder;
 }
