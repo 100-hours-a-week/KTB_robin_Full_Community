fcf808c131fa0b5bb9adc3bf056dc8d9589dc3aa
dragonwaterr <yongsu626@naver.com>
2025-11-05 21:15:34 +0900

    chore: 스프링 어플리케이션 클래스에 @EnableJpaAuditing 어노테이션 추가 -> 생성, 수정시간 자동 감지

--- src/main/java/ktb3/fullstack/week4/Week4Application.java
+++ src/main/java/ktb3/fullstack/week4/Week4Application.java
@@ -2,8 +2,10 @@ package ktb3.fullstack.week4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Week4Application {
 
 	public static void main(String[] args) {
