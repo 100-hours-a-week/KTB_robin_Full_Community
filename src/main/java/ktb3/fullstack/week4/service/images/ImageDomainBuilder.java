5f87e19b22ca18f9013b56cf6b868f687b86acb6
dragonwaterr <yongsu626@naver.com>
2025-11-12 17:46:02 +0900

    refactor: 연관관계 편의 메소드를 builder 메소드에 넣어서 누락 방지

--- src/main/java/ktb3/fullstack/week4/service/images/ImageDomainBuilder.java
+++ src/main/java/ktb3/fullstack/week4/service/images/ImageDomainBuilder.java
@@ -3,17 +3,21 @@ package ktb3.fullstack.week4.service.images;
 import ktb3.fullstack.week4.domain.images.PostImage;
 import ktb3.fullstack.week4.domain.images.ProfileImage;
 import ktb3.fullstack.week4.domain.posts.Post;
+import ktb3.fullstack.week4.domain.users.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ImageDomainBuilder {
 
-    public ProfileImage buildProfileImage(String profileImageUrl) {
-        return ProfileImage.builder()
+    public ProfileImage buildProfileImage(User user, String profileImageUrl) {
+        ProfileImage image = ProfileImage.builder()
                 .imageUrl(profileImageUrl)
                 .isPrimary(true)
                 .displayOrder(1)
                 .build();
+
+        image.linkUser(user);
+        return image;
     }
 
     // 한 장 등록
