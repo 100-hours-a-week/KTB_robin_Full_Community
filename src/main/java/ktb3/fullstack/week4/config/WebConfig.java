20d8c7bf7957510687579e1df32ac2ff7c3be876
dragonwaterr <yongsu626@naver.com>
2025-11-23 12:34:27 +0900

    refactor: WebConfig 클래스에서 사용되지않는 jwt 인터셉터 설정을 제거

--- src/main/java/ktb3/fullstack/week4/config/WebConfig.java
+++ src/main/java/ktb3/fullstack/week4/config/WebConfig.java
@@ -1,41 +1,11 @@
 package ktb3.fullstack.week4.config;
 
-import ktb3.fullstack.week4.auth.JwtAuthInterceptor;
-import ktb3.fullstack.week4.auth.JwtTokenProvider;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private static final String REFRESH_COOKIE_NAME = "refresh_token";
-
-    @Bean
-    public JwtTokenProvider jwtTokenProvider() {
-        return new JwtTokenProvider();
-    }
-
-    @Bean
-    public JwtAuthInterceptor jwtAuthInterceptor(JwtTokenProvider provider) {
-        return new JwtAuthInterceptor(provider, REFRESH_COOKIE_NAME);
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtAuthInterceptor(jwtTokenProvider()))
-                .addPathPatterns(
-                        "/users/**",
-                        "/posts/**"
-                )
-                .excludePathPatterns(
-                        "/auth/**",
-                        "/availability/**",
-                        "/users",
-                        "/error"
-                );
-    }
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
