2db8e64d8d7f945dc8e90d347e0cd8e10b0ae0b3
dragonwaterr <yongsu626@naver.com>
2025-11-29 15:03:41 +0900

    refactor: dto 생성자 추가

--- src/main/java/ktb3/fullstack/week4/dto/users/NicknameUpdateRequest.java
+++ src/main/java/ktb3/fullstack/week4/dto/users/NicknameUpdateRequest.java
@@ -1,9 +1,11 @@
 package ktb3.fullstack.week4.dto.users;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class NicknameUpdateRequest {
     @NotBlank
     private String newNickname;
--- src/main/java/ktb3/fullstack/week4/dto/users/PasswordUpdateRequest.java
+++ src/main/java/ktb3/fullstack/week4/dto/users/PasswordUpdateRequest.java
@@ -1,9 +1,11 @@
 package ktb3.fullstack.week4.dto.users;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class PasswordUpdateRequest {
     @NotBlank
     private String newPassword;
